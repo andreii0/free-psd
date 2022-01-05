7401a44cdefd9ac127e20fe8d571a3703ab83171
andreii0 <andrejboun@gmail.com>
2022-01-05 16:03:57 +0200

    section, main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(function(){
-$('.slider-inner').slick();
+
+$('.slider-inner').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    Infinity: false,
+
+});
 
 
 
